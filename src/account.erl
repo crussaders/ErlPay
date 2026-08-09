71551fee4ceee3d18c35b905822b6e0c602694d6
manishpandey <pandeym321@gmail.com>
2026-08-09 22:05:53 +0530

    Validation and Error Handling(concepts case, Guards, Tuples, errors)

--- src/account.erl
+++ src/account.erl
@@ -10,7 +10,7 @@
 -author("manishkumarpandey").
 
 %% API
--export([create/2, deposit/2, withdraw/2, balance/1]).
+-export([create/2, deposit/2, withdraw/2, balance/1, can_withdraw/2]).
 
 create(Id, Name) ->
   #{
@@ -19,13 +19,35 @@ create(Id, Name) ->
     balance => 0
   }.
 
-deposit(Account, Amount) ->
+deposit(Account, Amount) when Amount >0 ->
   CurrentBalance = maps:get(balance, Account),
-  Account#{balance => CurrentBalance + Amount}.
+  Account#{balance => CurrentBalance + Amount};
 
-withdraw(Account, Amount) ->
+deposit(_Account, _Amount) ->
+  {error, invalid_Amount}.
+
+%%% Amount > 0 is a guard
+withdraw(Account, Amount) when Amount > 0 ->
   CurrentBalance = maps:get(balance, Account),
-  Account#{balance => CurrentBalance - Amount}.
+
+  case CurrentBalance >= Amount of
+    true ->
+      NewAccount = Account#{balance => CurrentBalance - Amount},
+      {ok, NewAccount};
+
+    false ->
+      {error, insufficient_funds}
+  end;
+
+withdraw(_Account, _Amount) ->
+  {error, invalid_amount}.
+
+can_withdraw(Account, Amount) when Amount > 0 ->
+  CurrentBalance = maps:get(balance, Account),
+  CurrentBalance >= Amount;
+
+can_withdraw(_Account, _Amount) ->
+  false.
 
 balance(Account) ->
   maps:get(balance, Account).
