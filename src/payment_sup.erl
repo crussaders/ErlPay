b9a3fb4caa961aa231be7dd6a14c35c99971d500
manishpandey <pandeym321@gmail.com>
2026-08-16 17:56:04 +0530

    OTP Supervisor-is an erlang/OTP process whose responsibility is to monitor other process.

---
+++ src/payment_sup.erl
@@ -0,0 +1,38 @@
+%%%-------------------------------------------------------------------
+%%% @author manishkumarpandey
+%%% @copyright (C) 2026, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 16. Aug 2026 5:40 pm
+%%%-------------------------------------------------------------------
+-module(payment_sup).
+-author("manishkumarpandey").
+
+-behavior(supervisor).
+
+
+%% API
+-export([start_link/0]).
+-export([init/1]).
+
+start_link() ->
+  supervisor:start_link({local, ?MODULE},?MODULE,[]).
+
+init([]) ->
+  TransactionServer = #{
+    id => transaction_server,
+    start => {
+      transaction_server,
+      start_link,
+      []
+    },
+    restart => permanent,
+    shutdown => 5000,
+    type => worker
+  },
+
+  {ok , {
+    {one_for_one, 5, 10},
+    [TransactionServer]
+  }}.
