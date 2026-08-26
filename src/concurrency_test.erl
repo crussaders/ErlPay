9d9d4380f0f4cdd9a1157a0cc0a04120929b7623
manishpandey <pandeym321@gmail.com>
2026-08-26 21:20:42 +0530

    concurrency test code to spawn to process and send 2 transaction at the same time and test whether the concurrency is working

---
+++ src/concurrency_test.erl
@@ -0,0 +1,41 @@
+%%%-------------------------------------------------------------------
+%%% @author manishkumarpandey
+%%% @copyright (C) 2026, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 26. Aug 2026 9:10 pm
+%%%-------------------------------------------------------------------
+-module(concurrency_test).
+-author("manishkumarpandey").
+
+%% API
+-export([run/0]).
+
+run() ->
+  spawn(fun() ->
+    Result1 = payment_server:transfer(
+      1001,
+      1002,
+      8000
+    ),
+    io:format(
+      "Payment 1: ~p~n",
+      [Result1]
+    )
+        end),
+
+spawn(fun() ->
+  Result2 = payment_server:transfer(
+    1001,
+    1003,
+    7000
+  ),
+  io:format(
+    "Payment 1: ~p~n",
+    [Result2]
+  )
+      end),
+
+  ok.
+
